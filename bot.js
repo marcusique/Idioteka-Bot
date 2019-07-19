22eb7099a1b091c8b9aa2ccf6d6fd5186510b9b1
marcusique <markajtay@gmail.com>
2019-07-19 21:51:47 +0200

    test setup

--- bot.js
+++ bot.js
@@ -1,11 +1,52 @@
 const Telegraf = require('telegraf'),
+  Markup = require('telegraf/markup'),
   axios = require('axios'),
   cheerio = require('cheerio'),
+  { Random } = require('random-js'),
+  random = new Random(),
+  dateFormat = require('dateformat'),
   keys = require('./config/keys'),
   bot = new Telegraf(keys.telegramBotToken);
 
+function generateDate() {
+  let start = new Date(2006, 10, 1);
+  let end = new Date();
+  let date = random.date(start, end);
+  let newdate = dateFormat(date, 'yyyy/mm/dd');
+  return newdate;
+}
+
 bot.start(ctx => {
-  ctx.reply('hello');
+  let requestUrl = `${keys.URL}${generateDate()}`;
+  axios.get(requestUrl).then(html => {
+    let $ = cheerio.load(html.data);
+    let img = $('div.everiday-page-content-image > a > img').attr('src');
+    let title = $('div.als-text-container > p.before_list').text();
+    let caption = $('div.als-text-container > p')
+      .next()
+      .append('\n')
+      .text()
+      .trim();
+
+    ctx.replyWithPhoto(
+      img,
+      { caption: `${title}\n${caption}` },
+      Markup.inlineKeyboard([
+        Markup.urlButton('Grand Prix Report (Wikipedia)', `${requestUrl}`)
+      ]).extra()
+    );
+  });
 });
+
+// axios.get(URL).then(html => {
+//   let $ = cheerio.load(html.data);
+//   let img = $('div.everiday-page-content-image > a > img').attr('src');
+//   let title = $('div.als-text-container > p.before_list').text();
+//   let caption = $('div.als-text-container > p')
+//     .next()
+//     .append(' ')
+//     .text()
+//     .trim();
+// });
 
 bot.launch();
--- config/dev.js
+++ config/dev.js
@@ -1,3 +1,4 @@
 module.exports = {
-  telegramBotToken: '840465360:AAHJzqFm7soUP9FPXO-y3gj9LBHj_BRYvEA'
+  telegramBotToken: '840465360:AAHJzqFm7soUP9FPXO-y3gj9LBHj_BRYvEA',
+  URL: 'https://www.artlebedev.ru/kovodstvo/idioteka/'
 };

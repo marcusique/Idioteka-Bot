2447fc851c57cbdf7756b2d4be6eaadec96d7875
marcusique <markajtay@gmail.com>
2019-07-20 16:57:24 +0200

    Delete dev.js

--- config/dev.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  telegramBotToken: '840465360:AAHJzqFm7soUP9FPXO-y3gj9LBHj_BRYvEA',
-  URL: 'https://www.artlebedev.ru/kovodstvo/idioteka/',
-  mongoURI:
-    'mongodb://idioteka-dev:76MUMQqEzmpWKAAn@134.209.228.81:2222/idioteka-dev',
-  redisPort: '6379',
-  redisURI: '127.0.0.1',
-  redisPwd: 'admin'
-};

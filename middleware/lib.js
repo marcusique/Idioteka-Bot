8056585a670f8162faea98bafd318be1baaf0f54
marcusique <markajtay@gmail.com>
2019-12-08 15:59:32 +0100

    bug fixes

--- middleware/lib.js
+++ middleware/lib.js
@@ -5,7 +5,7 @@ const { Random } = require('random-js'),
 module.exports = {
   generateDate() {
     let start = new Date(2006, 10, 1);
-    let end = new Date();
+    let end = new Date(2019, 10, 31);
     let date = random.date(start, end);
     let newdate = dateFormat(date, 'yyyy/mm/dd');
     return newdate;
